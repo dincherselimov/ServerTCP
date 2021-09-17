1a7adadcf1147b22d4d7f208f01745d54c76c02f
dincher <80629557+dincherselimov@users.noreply.github.com>
2021-09-17 14:35:03 +0300

    Add files via upload

---
+++ Server/src/Config/Config.java
@@ -0,0 +1,49 @@
+package Config;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * This class uses Singleton method and has a connection to config.properties file
+ */
+public class Config {
+
+    private final int port;
+    private static Config instance = null;
+
+        //Define the config.properties file path
+        FileInputStream FIS;
+        {
+            try {
+                FIS = new FileInputStream("src/ConfigFiles/config.properties");
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        Properties prop = new Properties();
+
+        //Loading the config.properties file
+        private Config () throws IOException {
+        prop = new Properties();
+        prop.load(FIS);
+        FIS.close();
+//---------------------------------------------------------------------------------------------------//
+        port = Integer.parseInt(prop.getProperty("port"));
+    }
+
+        //Using Singleton method
+        public static Config getInstance() throws IOException {
+        if (instance == null) {
+            instance = new Config();
+        }
+        return instance;
+    }
+
+    //-------------------------------------------------------------------------------------------//
+        //Getters for strings
+        public int getPort(){return this.port;}
+
+}
--- Server/src/com/server/ServerTCP.java
+++ Server/src/com/server/ServerTCP.java
@@ -1,13 +1,20 @@
 package com.server;
+import Config.Config;
 import org.w3c.dom.ls.LSOutput;
 import javax.sound.midi.Soundbank;
 import java.io.*;
 import java.net.*;
 
+/**
+ * The ServerTCP class makes a connection with host 127.0.0.1 on port 80(what we have defined in config.properties)
+ * Opens a connection and communicates with the client
+ * The server accepts the information sent by the client
+ */
 public class ServerTCP {
     public static void main(String[] args) throws IOException {
 try {
-    ServerSocket serverSocket = new ServerSocket(80);
+    //connect to socket on host "localhost" and port 80
+    ServerSocket serverSocket = new ServerSocket(Config.getInstance().getPort());
 
     //returns the socket and establish a connection between server and client.
     Socket socket = serverSocket.accept();
@@ -18,12 +25,13 @@ try {
     String str = (String) dataInputStream.readUTF();
 
     //print a message
-    System.out.println("Message" + str);
+    System.out.println("Message" + " " +  str);
     //close the socket
     serverSocket.close();
-} catch (Exception e) {
-    e.printStackTrace();
-}
+          }
+        catch (Exception e) {
+         e.printStackTrace();
+        }
     }
 
 }
